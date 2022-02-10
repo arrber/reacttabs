5d376f85086e0292ec28607c8fbf8cf61eb214e6
arber <arberbreznica98@gmail.com>
2022-02-10 01:57:19 +0100

    adding files

--- src/App.js
+++ src/App.js
@@ -1,11 +1,62 @@
 import React, { Component } from 'react';
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+
 import './App.scss';
+
+import { DOMAINCARD } from './Data';
+import domainCard from './components/domainCard';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        
+      <div className="app">
+        <Tabs>
+          <TabList>
+            <Tab>Domains</Tab>
+            <Tab>Web Hosting</Tab>
+            <Tab>Dedicated Servers</Tab>
+            <Tab>Virtual Cloud Servers</Tab>
+            <Tab>WordPress Hosting </Tab>
+            <Tab>Email Hosting</Tab>
+            <Tab>VPS Hosting Servers</Tab>
+            <Tab>Free Hosting</Tab>
+          </TabList>
+
+          <TabPanel>
+            {
+              DOMAINCARD.domains.map((el, index) => {
+                return (
+                  <domainCard
+                    type={el.type}
+                    price={el.price}
+                    priceInfo={el.priceInfo}
+                  />
+                );
+              })
+            }
+          </TabPanel>
+          <TabPanel>
+            <h2>Any content 2</h2>
+          </TabPanel>
+          <TabPanel>
+            <h2>Any content 3</h2>
+          </TabPanel>
+          <TabPanel>
+            <h2>Any content 4</h2>
+          </TabPanel>
+          <TabPanel>
+            <h2>Any content 5</h2>
+          </TabPanel>
+          <TabPanel>
+            <h2>Any content 6</h2>
+          </TabPanel>
+          <TabPanel>
+            <h2>Any content 7</h2>
+          </TabPanel>
+          <TabPanel>
+            <h2>Any content 8</h2>
+          </TabPanel>
+        </Tabs>
       </div>
     );
   }
---
+++ src/Data.js
@@ -0,0 +1,52 @@
+export const DOMAINCARD = {
+    domains: [
+        {
+            type: ".COM",
+            price: "$5.99/yr",
+            priceInfo: "Instead of $10.99/yr",
+
+        },
+        {
+            type: ".AI",
+            price: "$55.99/yr",
+            priceInfo: "Instead of $10.99/yr",
+
+        },
+        {
+            type: ".NET",
+            price: "$7.99/yr",
+            priceInfo: "Instead of $10.99/yr",
+
+        },
+        {
+            type: ".HEALTH",
+            price: "$7.99/yr",
+            priceInfo: "Instead of $10.99/yr",
+
+        },
+        {
+            type: ".CO.UK",
+            price: "$3.99/yr",
+            priceInfo: "Instead of $10.99/yr",
+
+        },
+        {
+            type: ".ORG",
+            price: "$15.99/yr",
+            priceInfo: "Instead of $10.99/yr",
+
+        },
+        {
+            type: ".CO",
+            price: "$26.33/yr",
+            priceInfo: "Instead of $10.99/yr",
+
+        },
+        {
+            type: ".SEA",
+            price: "$26.33/yr",
+            priceInfo: "Instead of $10.99/yr",
+
+        }
+    ]
+}
